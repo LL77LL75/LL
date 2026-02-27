f4d53321d62466f03744106c5216decac75ca344
LL77LL75 <lancelimenze13@gmail.com>
2026-02-27 10:55:55 +0000

    w

--- CT08_06/project6_grading.py
+++ CT08_06/project6_grading.py
@@ -6,3 +6,19 @@ student_answers = {
     "alice": ["A", "C", "C", "D"],
     "bob": ["A", "B", "B", "D"]
 }
+avg_lst = []
+e = 0.0
+for i in student_answers:
+    score = 0
+    for j in range(len(answer_key)):
+        if student_answers[i][j] == answer_key[j]:
+            score += 1
+            total = len(answer_key)
+            percent = (score / total) * 100
+            avg_lst.append(percent)
+    print(f"{i} scored {percent}% on the quiz.")
+for i in avg_lst:
+    e += i
+total_avg = e / len(avg_lst)
+print(e)
+print(f"The average score for the quiz is {total_avg}%.") 
